96d81b5a6a2161e3571418f779a36510f715ffbc
cwarim-A <akoladequam9@gmail.com>
2023-07-18 17:40:30 +0100

    first commit

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -15,26 +15,20 @@ export const config = {
   };
   
 
-// export const POST = async (req) => {
-//   const file = await req.json();
-
-//   try {
-//     cloudinary.uploader.upload(file, (error, result) => {
-//       if (error) {
-//         console.error(error);
-//         res.status(500).json({ error: "Internal server error" });
-//       } else {
-//         const imageUrl = result.secure_url;
-//         res.status(200).json({ imageUrl });
-//       }
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-
-//   res.status(405).json({ error: "Method not allowed" });
-// };
+  export const POST = async (req) => {
+    try {
+      const formData = await req.formData();
+      const File = formData.get("file") ;
+      console.log(File)
+      const result = await cloudinary.uploader.upload(File);
+      const imageUrl = result.secure_url;
+      return new Response(JSON.stringify(imageUrl), { status: 201 });
+    } catch (error) {
+      console.error(error);
+      return new Response("Failed to upload image to cloudinary", { status: 400 });
+    }
+  };
+  
 
 // export const POST = async (request) => {
 //     try {
@@ -49,28 +43,28 @@ export const config = {
 //     }
 //   };
 
-const handler = async (req,res)=>{
-    try {
-        const file = await new Promise((resolve,reject)=>{
-            const form = formidable();
-            form.parse(req,(err,fields,files)=>{
-                if(err) return reject(err);
-            });
-            form.on("file",(formName, file)=>{
-                resolve(file)
-            });
-        });
-        const data = await cloudinary.upload.unsigned_upload(
-            file.filepath,
-            process.env.UPLOAD_PRESET
-        );
-        res.status(200).json({fileUrl: data.secure_url})
-    } catch(error){
-        console.error(error);
-        res.status(500).json({message:"server upload error"})
-    }
-}
+// const handler = async (req,res)=>{
+//     try {
+//         const file = await new Promise((resolve,reject)=>{
+//             const form = formidable();
+//             form.parse(req,(err,fields,files)=>{
+//                 if(err) return reject(err);
+//             });
+//             form.on("file",(formName, file)=>{
+//                 resolve(file)
+//             });
+//         });
+//         const data = await cloudinary.upload.unsigned_upload(
+//             file.filepath,
+//             process.env.UPLOAD_PRESET
+//         );
+//         res.status(200).json({fileUrl: data.secure_url})
+//     } catch(error){
+//         console.error(error);
+//         res.status(500).json({message:"server upload error"})
+//     }
+// }
 
 
-export {handler as POST,handler as GET}
+// export {handler as POST,handler as GET}
   
